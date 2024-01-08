7eac384334bbe7a6bff0b073bcae7256c70aa2cd
AbbadIrfan <134639359+AbbadIrfan@users.noreply.github.com>
2024-01-08 18:56:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "48699529480" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['923462740696'] 
+global.prems = ['923462740696']
+global.allowed = ['923107040696', '923462740696']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,11 +50,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '😈🔥 𝕯𝖊𝖆𝖙𝖍 𝕮𝖆𝖑𝖑𝖊𝖗 🔥😈'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.packname = '😈🔥 𝕯𝖊𝖆𝖙𝖍 𝕮𝖆𝖑𝖑𝖊𝖗 🔥😈' 
+global.author = '😈🔥 𝕯𝖊𝖆𝖙𝖍 𝕮𝖆𝖑𝖑𝖊𝖗 🔥😈' 
+global.menuvid = 'https://telegra.ph/file/8d169220c7fc51f91fdee.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
